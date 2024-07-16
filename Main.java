86896b9b0a7b4f770a8d936088976693df6a0d45
Teji420 <t.singh.34@student.scu.edu.au>
2024-07-16 12:37:51 +1000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class HealthProfessional{
+    private int id;
+    private string name;
+    private string specialty;
+
+
+    //  here i am gonna introduce the default constrcutor .//
+    public HealthProfessional() {
+
+    }
+    public HealthProfessional(int id, string name, string specialty){
+        this.id = id;
+        this.name = name;
+        this.specialty= specialty;
+
+    }
+
+
+}
